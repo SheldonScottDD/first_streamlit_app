b73e98deb5afcc83a69a66ca577ef8ec024f9a09
Sheldon Scott <104961355+SheldonScottDD@users.noreply.github.com>
2022-05-27 15:17:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
 
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header('Breakfast Favourites')
-streamlit.text('🥣omega 2 & Blueberry Oatmeal')
-streamlit.text('🥑🍞Avocado Toast')
+streamlit.header("Breakfast Favourites")
+streamlit.text("🥣omega 2 & Blueberry Oatmeal")
+streamlit.text("🥑🍞Avocado Toast")
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 
+#import pandas
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
