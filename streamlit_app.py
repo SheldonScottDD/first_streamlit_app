f94813aeca8400b746eb6e64e73a8432f044c121
Sheldon Scott <104961355+SheldonScottDD@users.noreply.github.com>
2022-05-27 15:14:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
 
+import streamlit
+
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣omega 2 & Blueberry Oatmeal')
+streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
