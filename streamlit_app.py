241795367c14db124423fe16d3567db29cae4f93
Sheldon Scott <104961355+SheldonScottDD@users.noreply.github.com>
2022-05-30 11:38:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ streamlit.dataframe(my_fruit_list)
 
 streamlit.header('Fruityvice Fruit Advice!')
 
-miport requests
+import requests
 fruityvice_response = requests.get("https://fruitvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
